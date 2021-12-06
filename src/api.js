2f88c3393298e97c1e837c698b82a62bd68f1a84
ahmedhassan2k <ah2k.dev@gmail.com>
2021-12-07 01:01:55 +0500

    Put and Delete methods

--- src/api.js
+++ src/api.js
@@ -46,10 +46,52 @@ router.post('/books', (req,res)=>{
 })
 
 router.put('/books/:id', (req,res)=>{
-    
+     const { id } = req.params;
+    const {
+        title,
+        isbn,
+        pageCount,
+        publishedDate,
+        thumbnailUrl,
+        shortDescription,
+        longDescription,
+        status,
+        authors,
+        categories
+    } = req.body;
+
+    let book = booksDirectory.find(b => b.isbn === id);
+    if (!book) return res.status(404).send('Book does not exist');
+    const updateField = (val, prev) => !val ? prev : val;
+
+    const updatedBook = {
+        ...book,
+        title: updateField(title, book.title),
+        isbn: updateField(isbn, book.isbn),
+        pageCount: updateField(pageCount, book.pageCount),
+        publishedDate: updateField(publishedDate, book.publishedDate),
+        thumbnailUrl: updateField(thumbnailUrl, book.thumbnailUrl),
+        shortDescription: updateField(shortDescription, book.shortDescription),
+        longDescription: updateField(longDescription, book.longDescription),
+        status: updateField(status, book.status),
+        authors: updateField(authors, book.authors),
+        categories: updateField(categories, book.categories),
+    };
+
+    const bookIndex = booksDirectory.findIndex(b => b.isbn === book.isbn);
+    booksDirectory.splice(bookIndex, 1, updatedBook);
+
+    res.status(200).send(updatedBook);
 })
 
 router.delete('/books/:id', (req,res)=>{
-    
+    const { id } = req.params;
+
+    let book = booksDirectory.find(b => b.isbn === id);
+    if (!book) return res.status(404).send('Book does not exist');
+
+    booksDirectory = booksDirectory.filter(b => b.isbn !== id);
+
+    res.send('Success');
 })
 module.exports = router;
